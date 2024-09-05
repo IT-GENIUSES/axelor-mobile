210f45aadc41a3acbde872ebe552bc7fff9624f6
vhu-axelor <146069039+vhu-axelor@users.noreply.github.com>
2024-09-05 14:12:34 +0200

    feat: add leave requester screen (#683)
    * RM#82780

--- packages/apps/hr/src/models/objectFields.ts
+++ packages/apps/hr/src/models/objectFields.ts
@@ -149,6 +149,9 @@ export const hr_modelAPI: ObjectFields = {
           .of(schemaContructor.subObject()),
         product: schemaContructor.subObject('name'),
         timesheetImputationSelect: schemaContructor.number(),
+        leaveLineList: schemaContructor
+          .array()
+          .of(schemaContructor.subObject()),
       }),
     ),
   }),
@@ -175,5 +178,9 @@ export const hr_modelAPI: ObjectFields = {
     validatedBy: schemaContructor.subObject('name'),
     refusalDateTime: schemaContructor.string(),
     refusedBy: schemaContructor.subObject('name'),
+  }),
+  hr_leaveReason: schemaContructor.object({
+    name: schemaContructor.string(),
+    unitSelect: schemaContructor.number(),
   }),
 };
